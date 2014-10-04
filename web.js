b9471d72c3eea6f1ba7f177f33501d67a2c02799
Rujia Zha <rujiazha@gmail.com>
2014-10-03 21:35:50 -0400

    added db

--- web.js
+++ web.js
@@ -8,7 +8,16 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/proj2');
+
+var connection_string = 'localhost:27017/proj2';
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+var db = monk(connection_string);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
